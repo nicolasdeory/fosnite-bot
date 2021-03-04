8fdbe849e950b45b22dd77a278ee11616daaf01c
Nicolás de Ory <deorynicolas@gmail.com>
2021-03-05 00:54:56 +0100

    No sandbox mode venom

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { create } from "venom-bot";
 import bot from "./bot";
 
-create('test2')
+create('test2',(base64Qrimg, asciiQR, attempts) => {}, (statusSession, session) => {}, { useChrome: false, browserArgs: ['--no-sandbox'] })
 .then((client) => bot(client))
 .catch((error) => console.log(error));
