b27fef4a04eb02cc4ef80f2ceb0b301cbb86f876
Nicolás de Ory <deorynicolas@gmail.com>
2021-03-05 01:19:51 +0100

    Added mini server

--- build/index.js
+++ build/index.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var venom_bot_1 = require("venom-bot");
 var bot_1 = __importDefault(require("./bot"));
-venom_bot_1.create('test2')
+var express_1 = __importDefault(require("express"));
+venom_bot_1.create('test2', function (base64Qrimg, asciiQR, attempts) { }, function (statusSession, session) { }, { useChrome: false, browserArgs: ['--no-sandbox'] })
     .then(function (client) { return bot_1.default(client); })
     .catch(function (error) { return console.log(error); });
+var app = express_1.default();
+app.listen(process.env.PORT, function () { return console.log("server listening"); });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 import { create } from "venom-bot";
 import bot from "./bot";
 
+import express from "express";
+
 create('test2',(base64Qrimg, asciiQR, attempts) => {}, (statusSession, session) => {}, { useChrome: false, browserArgs: ['--no-sandbox'] })
 .then((client) => bot(client))
 .catch((error) => console.log(error));
+
+const app = express();
+app.get('/', (req, res) =>
+{
+    res.send("Fornique bot is running");
+});
+
+app.listen(process.env.PORT, () => console.log("server listening"));
